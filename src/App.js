f8f35994d6eacc583ad881700c1a6c8b0c8f2812
IvanGTR43 <ivan_tr43@outlook.com>
2020-12-03 18:31:59 -0600

    "Inicio"

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Ivan Gabriel Tacuapan Reyes
+          Ivan Gabriel
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
